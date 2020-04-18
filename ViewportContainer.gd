89df4476c11fc86cebc9610b9f9e6437c3c03c44
danielcannell <danielcannell@googlemail.com>
2020-04-18 20:03:27 +0100

    Second attempt at fixing input events

--- ViewportContainer.gd
+++
@@ -1,6 +0,0 @@
-extends ViewportContainer
-
-func _input(event):
-    # See: https://github.com/godotengine/godot/issues/17326
-    # and: https://github.com/godotengine/godot/issues/31802
-    $Viewport.unhandled_input(event)
