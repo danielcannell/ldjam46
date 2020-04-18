b59bc18fb904893f5f45658edba2d13c924b3db6
Jamie Wood <jamie.wood70@ntlworld.com>
2020-04-18 12:01:35 +0100

    Add map scene

--- Main.gd
+++ Main.gd
@@ -5,7 +5,7 @@ func _ready():
     pass
 
 
-func _process(delta):
+func _process(_delta):
     var size := get_viewport_rect().size - Vector2(180, 0)
     $ViewportContainer.rect_size = size
     $ViewportContainer/Viewport.size = size
