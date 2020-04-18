0b6611202d694e4c6b4e6ff2316c7e21a5d7239c
danielcannell <danielcannell@googlemail.com>
2020-04-18 21:07:09 +0100

    Remove some unnecessary code

--- Playfield/Towers/Tower.gd
+++ Playfield/Towers/Tower.gd
@@ -30,11 +30,8 @@ func _init(kind: String, pos: Vector2):
     sprite = Sprite.new()
     sprite.texture = tower_def["image"]
     add_child(sprite)
-    
-    var image_size = tower_def["image"].get_size()
-    
+
     sprite.region_enabled = true
-    sprite.region_rect = Rect2(0, 0, image_size.x, 0)
     adjust_sprite()
 
 
