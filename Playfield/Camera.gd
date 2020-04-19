d0e2b7ac4347b1e34f21c2731aa9f52a10219f01
danielcannell <danielcannell@googlemail.com>
2020-04-19 12:55:18 +0100

    Limit camera to the map

--- Playfield/Camera.gd
+++ Playfield/Camera.gd
@@ -1,18 +1,8 @@
 extends Camera2D
 
 
-const MAX_ZOOM = 1
+const MAX_ZOOM = 0.4
 const MIN_ZOOM = 0.1
-const DRAG_MARGIN = 0.2
-
-
-func _init():
-    drag_margin_left = DRAG_MARGIN
-    drag_margin_right = DRAG_MARGIN
-    drag_margin_top = DRAG_MARGIN
-    drag_margin_bottom = DRAG_MARGIN
-    drag_margin_h_enabled = true
-    drag_margin_v_enabled = true
 
 
 func _unhandled_input(event):
